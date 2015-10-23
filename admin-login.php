d2831d889048f4f702138e09d6ab979123c16b65
Philip Kahn <tigerhawkvok@gmail.com>
2015-10-23 14:46:01 -0700

    Point to correct file

--- admin-login.php
+++ admin-login.php
@@ -34,7 +34,7 @@
 ini_set("display_errors",1);
 ini_set("log_errors",1);
 error_reporting(E_ALL);
-       require_once("CONFIG.php");
+       require_once("DB_CONFIG.php");
        require_once("admin/login.php");
        echo $login_output;
        ?>
