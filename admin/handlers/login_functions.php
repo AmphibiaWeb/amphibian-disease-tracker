64f2cbb05fe289f8f2d34cba19925cda1c9941c0
Philip Kahn <tigerhawkvok@gmail.com>
2015-10-26 14:17:22 -0700

    Stored

--- admin/handlers/login_functions.php
+++ admin/handlers/login_functions.php
@@ -1124,7 +1124,7 @@ class UserFunctions extends DBHelper
                         }
                         }
                     } else {
-                        return array(false,'status' => false,'message' => 'Sorry, your username or password is incorrect.','error' => 'Bad Password', "detail" => $hash->verifyHash($pw, $data, null, null, null, true), "given" => $pw);
+                        return array(false,'status' => false,'message' => 'Sorry, your username or password is incorrect.','error' => 'Bad Password', "detail" => $hash->verifyHash($pw, $data, null, null, null, true), "given" => $pw, "stored_data_reference" => $data);
                     }
                 # end good username loop
                 } else {
