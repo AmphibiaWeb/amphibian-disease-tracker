49d9a3fbf3803a45e4f133f7732e28584aa245b0
Philip Kahn <tigerhawkvok@gmail.com>
2015-10-30 02:24:53 -0700

    More fixing

--- helpers/excelHelper.php
+++ helpers/excelHelper.php
@@ -82,7 +82,7 @@ function excelToPhp($filePath) {
 */
 function excelToArray($filePath, $header=true){
     //Create excel reader after determining the file type
-    $inputFileName = dirname(__FILE__) . $filePath;
+    $inputFileName =  $filePath;
     /**  Identify the type of $inputFileName  **/
     $inputFileType = PHPExcel_IOFactory::identify($inputFileName);
     /**  Create a new Reader of the type that has been identified  **/
@@ -139,10 +139,11 @@ $do = isset($_REQUEST['action']) ? strtolower($_REQUEST['action']) : "NO_PROVIDE
 
 switch ($do) {
 case "parse":
-    if(!file_exists(dirname(__FILE__) . $_REQUEST["path"])) {
+    $validatedPath = dirname(__FILE__) . "/" .$_REQUEST["path"]; 
+    if(!file_exists($validatedPath)) {
         returnAjax(array(
             "status" => false,
-            "error" => "Non-existant file '".$_REQUEST["path"]."'",
+            "error" => "Non-existant file '".$validatedPath."'",
             "human_error" => "There was a problem validating your file. Please try again."
         ));
     }
