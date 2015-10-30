097f578cdbefddc161651865e3716cc7dc28151e
Philip Kahn <tigerhawkvok@gmail.com>
2015-10-29 22:46:39 -0700

    Modify subtree to work with current project

--- helpers/js-dragdrop/client-bootstrapper.coffee
+++ helpers/js-dragdrop/client-bootstrapper.coffee
@@ -6,6 +6,102 @@
 ###
 
 
+unless window.dropperParams?
+  window.dropperParams = new Object()
+
+
+
+window.dropperParams.postUploadHandler = (file, result) ->
+  ###
+  # The callback function for handleDragDropImage
+  #
+  # The "file" object contains information about the uploaded file,
+  # such as name, height, width, size, type, and more. Check the
+  # console logs in the demo for a full output.
+  #
+  # The result object contains the results of the upload. The "status"
+  # key is true or false depending on the status of the upload, and
+  # the other most useful keys will be "full_path" and "thumb_path".
+  #
+  # When invoked, it calls the "self" helper methods to actually do
+  # the file sending.
+  ###
+  # Clear out the file uploader
+  window.dropperParams.dropzone.removeAllFiles()
+
+  if typeof result isnt "object"
+    console.error "Dropzone returned an error - #{result}"
+    window.toastStatusMessage("<strong>Error</strong> There was a problem with the server handling your image. Please try again.", "danger", "#profile_conversation_wrapper")
+    return false
+  unless result.status is true
+    # Yikes! Didn't work
+    result.human_error ?= "There was a problem uploading your image."
+    window.toastStatusMessage("<strong>Error</strong> #{result.human_error}", "danger", "#profile_conversation_wrapper")
+    console.error("Error uploading!",result)
+    return false
+  try
+    console.info "Server returned the following result:", result
+    console.info "The script returned the following file information:", file
+    # EG, an S3 bucket
+    pathPrefix = ""
+    # Replace full_path and thumb_path with "wrote"
+    result.full_path = result.wrote_file
+    result.thumb_path = result.wrote_thumb
+    mediaType = result.mime_provided.split("/")[0]
+    serverProcessFlag = "<!--FlagProcessImage::#{mediaType}::#{pathPrefix}#{result.full_path}::#{pathPrefix}#{result.thumb_path}-->"
+    html = switch mediaType
+      when "image" then """#{serverProcessFlag}
+        <div class='message-media'>
+          <a href="#{pathPrefix}#{result.full_path}" class="newwindow">
+            <img src="#{pathPrefix}#{result.thumb_path}" />
+          </a>
+          <p class="text-muted">Click the thumbnail for a full-sized image (#{file.name})</p>
+        </div>
+        """
+      when "audio" then """#{serverProcessFlag}
+      <div class="message-media">
+        <audio src="#{pathPrefix}#{result.full_path}" controls preload="auto">
+          <img src="#{pathPrefix}#{result.thumb_path}" alt="Audio Thumbnail" class="img-responsive" />
+          <p>
+            Your browser doesn't support the HTML5 <code>audio</code> element.
+            Please download the file below.
+          </p>
+        </audio>
+        <p class="text-muted">
+          (<a href="#{pathPrefix}#{result.full_path}" class="newwindow" download="#{file.name}">
+            Original Media
+          </a>)
+        </p>
+      </div>
+      """
+      when "video" then """#{serverProcessFlag}
+      <div class="message-media">
+        <video src="#{pathPrefix}#{result.full_path}" controls preload="auto">
+          <img src="#{pathPrefix}#{result.thumb_path}" alt="Video Thumbnail" class="img-responsive" />
+          <p>
+            Your browser doesn't support the HTML5 <code>video</code> element.
+            Please download the file below.
+          </p>
+        </video>
+        <p class="text-muted">
+          (<a href="#{pathPrefix}#{result.full_path}" class="newwindow" download="#{file.name}">
+            Original Media
+          </a>)
+        </p>
+      </div>
+      """
+    window.dropperParams.richDisplay = html
+    $("#profile_messages").append(html) ## Test, displays only
+    realPath = if file.size < 5*1024*1024 or mediaType isnt "image" then "#{pathPrefix}#{result.full_path}" else "#{pathPrefix}#{result.thumb_path}"
+    # Actually send the message    
+  catch e
+    console.error("There was a problem with upload post-processing - #{e.message}")
+    console.warn("Using",file.name,result)
+    window.toastStatusMessage("<strong>Error</strong> Your upload completed, but we couldn't post-process it.", "danger", "#profile_conversation_wrapper")
+  false
+
+
+
 ###
 # Watch DOM for element availability.
 #
@@ -71,21 +167,19 @@ $ ->
   # Configuration
   console.info "Configuring dropper parameters"
   window.dropperParams ?= new Object()
-  window.dropperParams.metaPath = "/media-uploader/"
-  window.dropperParams.uploadPath = "uploaded/"
-  window.dropperParams.dependencyPath = "/media-uploader/bower_components/"
+  window.dropperParams.metaPath = "/helpers/js-dragdrop/"
+  window.dropperParams.uploadPath = "#{window.dropperParams.metaPath}uploaded/"
+  window.dropperParams.dependencyPath = "#{window.dropperParams.metapath}bower_components/"
   window.dropperParams.showProgress = true
-  window.dropperParams.dropTargetSelector ?= "#profile_new_message"
+  window.dropperParams.dropTargetSelector ?= "#file-uploader"
   # Add a click target
   uploadButton = """
-  <button class="upload-image media-uploader btn btn primary" id="do-upload-image"><span class="icon-batch-image" style="position:relative; right:3px;"></span></button>
+  <button class="upload-image media-uploader btn btn primary" id="do-upload-file"><span class="glyphicon glyphicon-cloud-upload"></span></button>
   """
-  window.dropperParams.clickTargets = ["#do-upload-image"]
+  window.dropperParams.clickTargets = ["#do-upload-file"]
+  window.dropperParams.mimeTypes = "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet,application/vnd.ms-excel,text/csv,application/zip,application/x-7z-compressed,image/*"
 
   console.log window.dropperParams
-
-  # Load Bootstrap's JS
-  loadJS dropperParams.bootstrapPath
   # We shouldn't actually instantiate the dropper until the element
   # exists
   ready dropperParams.dropTargetSelector, (element) ->
--- helpers/js-dragdrop/client-upload.js
+++ helpers/js-dragdrop/client-upload.js
@@ -6,15 +6,80 @@
  * `integration.coffee` after this comment.
  */
 
-
-/*
- * Watch DOM for element availability.
- *
- * Directly lifted from
- * http://ryanmorr.com/using-mutation-observers-to-watch-for-element-availability/
- */
-
 (function() {
+  if (window.dropperParams == null) {
+    window.dropperParams = new Object();
+  }
+
+  window.dropperParams.postUploadHandler = function(file, result) {
+
+    /*
+     * The callback function for handleDragDropImage
+     *
+     * The "file" object contains information about the uploaded file,
+     * such as name, height, width, size, type, and more. Check the
+     * console logs in the demo for a full output.
+     *
+     * The result object contains the results of the upload. The "status"
+     * key is true or false depending on the status of the upload, and
+     * the other most useful keys will be "full_path" and "thumb_path".
+     *
+     * When invoked, it calls the "self" helper methods to actually do
+     * the file sending.
+     */
+    var e, html, mediaType, pathPrefix, realPath, serverProcessFlag;
+    window.dropperParams.dropzone.removeAllFiles();
+    if (typeof result !== "object") {
+      console.error("Dropzone returned an error - " + result);
+      window.toastStatusMessage("<strong>Error</strong> There was a problem with the server handling your image. Please try again.", "danger", "#profile_conversation_wrapper");
+      return false;
+    }
+    if (result.status !== true) {
+      if (result.human_error == null) {
+        result.human_error = "There was a problem uploading your image.";
+      }
+      window.toastStatusMessage("<strong>Error</strong> " + result.human_error, "danger", "#profile_conversation_wrapper");
+      console.error("Error uploading!", result);
+      return false;
+    }
+    try {
+      console.info("Server returned the following result:", result);
+      console.info("The script returned the following file information:", file);
+      pathPrefix = "";
+      result.full_path = result.wrote_file;
+      result.thumb_path = result.wrote_thumb;
+      mediaType = result.mime_provided.split("/")[0];
+      serverProcessFlag = "<!--FlagProcessImage::" + mediaType + "::" + pathPrefix + result.full_path + "::" + pathPrefix + result.thumb_path + "-->";
+      html = (function() {
+        switch (mediaType) {
+          case "image":
+            return serverProcessFlag + "\n<div class='message-media'>\n  <a href=\"" + pathPrefix + result.full_path + "\" class=\"newwindow\">\n    <img src=\"" + pathPrefix + result.thumb_path + "\" />\n  </a>\n  <p class=\"text-muted\">Click the thumbnail for a full-sized image (" + file.name + ")</p>\n</div>";
+          case "audio":
+            return serverProcessFlag + "\n<div class=\"message-media\">\n  <audio src=\"" + pathPrefix + result.full_path + "\" controls preload=\"auto\">\n    <img src=\"" + pathPrefix + result.thumb_path + "\" alt=\"Audio Thumbnail\" class=\"img-responsive\" />\n    <p>\n      Your browser doesn't support the HTML5 <code>audio</code> element.\n      Please download the file below.\n    </p>\n  </audio>\n  <p class=\"text-muted\">\n    (<a href=\"" + pathPrefix + result.full_path + "\" class=\"newwindow\" download=\"" + file.name + "\">\n      Original Media\n    </a>)\n  </p>\n</div>";
+          case "video":
+            return serverProcessFlag + "\n<div class=\"message-media\">\n  <video src=\"" + pathPrefix + result.full_path + "\" controls preload=\"auto\">\n    <img src=\"" + pathPrefix + result.thumb_path + "\" alt=\"Video Thumbnail\" class=\"img-responsive\" />\n    <p>\n      Your browser doesn't support the HTML5 <code>video</code> element.\n      Please download the file below.\n    </p>\n  </video>\n  <p class=\"text-muted\">\n    (<a href=\"" + pathPrefix + result.full_path + "\" class=\"newwindow\" download=\"" + file.name + "\">\n      Original Media\n    </a>)\n  </p>\n</div>";
+        }
+      })();
+      window.dropperParams.richDisplay = html;
+      $("#profile_messages").append(html);
+      realPath = file.size < 5 * 1024 * 1024 || mediaType !== "image" ? "" + pathPrefix + result.full_path : "" + pathPrefix + result.thumb_path;
+    } catch (_error) {
+      e = _error;
+      console.error("There was a problem with upload post-processing - " + e.message);
+      console.warn("Using", file.name, result);
+      window.toastStatusMessage("<strong>Error</strong> Your upload completed, but we couldn't post-process it.", "danger", "#profile_conversation_wrapper");
+    }
+    return false;
+  };
+
+
+  /*
+   * Watch DOM for element availability.
+   *
+   * Directly lifted from
+   * http://ryanmorr.com/using-mutation-observers-to-watch-for-element-availability/
+   */
+
   
 (function(win){
     'use strict';
@@ -66,23 +131,24 @@
 
 })(this);
 ;
+
   $(function() {
     var base, uploadButton;
     console.info("Configuring dropper parameters");
     if (window.dropperParams == null) {
       window.dropperParams = new Object();
     }
-    window.dropperParams.metaPath = "/media-uploader/";
-    window.dropperParams.uploadPath = "uploaded/";
-    window.dropperParams.dependencyPath = "/media-uploader/bower_components/";
+    window.dropperParams.metaPath = "/helpers/js-dragdrop/";
+    window.dropperParams.uploadPath = window.dropperParams.metaPath + "uploaded/";
+    window.dropperParams.dependencyPath = window.dropperParams.metapath + "bower_components/";
     window.dropperParams.showProgress = true;
     if ((base = window.dropperParams).dropTargetSelector == null) {
-      base.dropTargetSelector = "#profile_new_message";
+      base.dropTargetSelector = "#file-uploader";
     }
-    uploadButton = "<button class=\"upload-image media-uploader btn btn primary\" id=\"do-upload-image\"><span class=\"icon-batch-image\" style=\"position:relative; right:3px;\"></span></button>";
-    window.dropperParams.clickTargets = ["#do-upload-image"];
+    uploadButton = "<button class=\"upload-image media-uploader btn btn primary\" id=\"do-upload-file\"><span class=\"glyphicon glyphicon-cloud-upload\"></span></button>";
+    window.dropperParams.clickTargets = ["#do-upload-file"];
+    window.dropperParams.mimeTypes = "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet,application/vnd.ms-excel,text/csv,application/zip,application/x-7z-compressed,image/*";
     console.log(window.dropperParams);
-    loadJS(dropperParams.bootstrapPath);
     return ready(dropperParams.dropTargetSelector, function(element) {
       console.info(dropperParams.dropTargetSelector + " is ready, binding");
       $(window.dropperParams.dropTargetSelector).parent().after(uploadButton);
