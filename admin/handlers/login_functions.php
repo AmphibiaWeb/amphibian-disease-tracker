b0af202636bc712a568775eebdcd52d4206b57de
Philip Kahn <tigerhawkvok@gmail.com>
2016-01-30 15:00:30 -0800

    Back to AES-256-CBC-HMAC-SHA1 ...

--- admin/handlers/login_functions.php
+++ admin/handlers/login_functions.php
@@ -2040,7 +2040,7 @@ class UserFunctions extends DBHelper
                 if ($match_token != $verify) {
                     # The computed token doesn't match the provided one
                     $testPass = "123abc";
-                    $method = "AES-128-CBC";
+                    $method = "AES-256-CBC-HMAC-SHA1";
                     $foo = openssl_encrypt("FooBar", $method, $testPass, 0, $this->getUserSeed());
                     $bar = openssl_decrypt($foo, $method, $testPass, 0, $this->getUserSeed());
                     throw(new Exception('Invalid reset tokens (got '.$string.' and match '.$match_token.' from '.$salt.' and '.$secret.' [input->'.$key.':'.$verify.' with iv '.$this->getUserSeed().']). Tested '.$foo.' decoding to '.$bar.' from ' .print_r(openssl_get_cipher_methods(),true) . openssl_error_string()));
