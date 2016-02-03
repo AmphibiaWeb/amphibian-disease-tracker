ee6cf9c59fca70f8f2e58dcbf772193f32124436
Philip Kahn <tigerhawkvok@gmail.com>
2016-02-02 23:44:54 -0800

    Don't explode unless has a delimeter

--- api.php
+++ api.php
@@ -219,10 +219,17 @@ function doAWebValidate($get) {
         if($k == 0) continue; # Prevent match on "genus"
         $genus = strtolower($entry[3]);
         $genusList[] = $genus;
-        $synon = explode(",", strtolower($entry[8]));
-        foreach($synon as $oldName) {
-            $key = trim($oldName);
-            $synonymList[$key] = $k;
+        $synonEntry = strtolower($entry[8]);
+        if(!empty($synonEntry)) {
+            if(strpos($synonEntry, ",") !== false) {
+                $synon = explode(",", $synonEntry);
+            } else {
+                $synon = array($synonEntry);
+            }        
+            foreach($synon as $oldName) {
+                $key = trim($oldName);
+                $synonymList[$key] = $k;
+            }
         }
     }
     # First check: Does the genus exist?
