9c13cbe6e0bcff4c908162e414b4068672f8e101
Philip Kahn <tigerhawkvok@gmail.com>
2016-07-13 15:43:34 -0700

    Restore project.php to pre-vulcanized version

--- project.php
+++ project.php
@@ -44,8 +44,71 @@ $loginStatus = getLoginState();
     <link rel="icon" type="image/png" sizes="128x128" href="assets/favicon128.png" />
     <link rel="icon" type="image/png" sizes="256x254" href="assets/favicon256.png" />
     <link rel="icon" type="image/png" sizes="512x512" href="assets/favicon512.png" />
-    <link rel="icon" type="image/png" sizes="1024x1024" href="assets/favicon1024.png" />    
+    <link rel="icon" type="image/png" sizes="1024x1024" href="assets/favicon1024.png" />
 
+    <script src="bower_components/webcomponentsjs/webcomponents-lite.min.js"></script>
+
+    <link rel="import" href="bower_components/polymer/polymer.html"/>
+
+
+    <link rel="import" href="bower_components/paper-toggle-button/paper-toggle-button.html"/>
+    <link rel="import" href="bower_components/paper-checkbox/paper-checkbox.html"/>
+    <link rel="import" href="bower_components/paper-toast/paper-toast.html"/>
+    <link rel="import" href="bower_components/paper-input/paper-input.html"/>
+    <link rel="import" href="bower_components/paper-input/paper-textarea.html"/>
+    <link rel="import" href="bower_components/paper-spinner/paper-spinner.html"/>
+    <link rel="import" href="bower_components/paper-slider/paper-slider.html"/>
+    <link rel="import" href="bower_components/paper-menu/paper-menu.html"/>
+    <link rel="import" href="bower_components/paper-card/paper-card.html"/>
+
+    <link rel="import" href="bower_components/paper-dialog/paper-dialog.html"/>
+    <link rel="import" href="bower_components/paper-radio-group/paper-radio-group.html"/>
+    <link rel="import" href="bower_components/paper-radio-button/paper-radio-button.html"/>
+
+    <link rel="import" href="bower_components/paper-dialog-scrollable/paper-dialog-scrollable.html"/>
+    <link rel="import" href="bower_components/paper-button/paper-button.html"/>
+    <link rel="import" href="bower_components/paper-icon-button/paper-icon-button.html"/>
+    <link rel="import" href="bower_components/paper-fab/paper-fab.html"/>
+    <link rel="import" href="bower_components/paper-item/paper-item.html"/>
+    <link rel="import" href="bower_components/paper-material/paper-material.html"/>
+
+    <link rel="import" href="bower_components/gold-email-input/gold-email-input.html"/>
+    <link rel="import" href="bower_components/gold-phone-input/gold-phone-input.html"/>
+
+    <link rel="import" href="bower_components/iron-form/iron-form.html"/>
+    <link rel="import" href="bower_components/iron-autogrow-textarea/iron-autogrow-textarea.html"/>
+
+    <link rel="import" href="bower_components/font-roboto/roboto.html"/>
+    <link rel="import" href="bower_components/iron-icons/iron-icons.html"/>
+    <link rel="import" href="bower_components/iron-icons/image-icons.html"/>
+    <link rel="import" href="bower_components/iron-icons/social-icons.html"/>
+    <link rel="import" href="bower_components/iron-icons/communication-icons.html"/>
+    <link rel="import" href="bower_components/iron-icons/editor-icons.html"/>
+
+    <link rel="import" href="bower_components/neon-animation/neon-animation.html"/>
+
+    <link rel="import" href="bower_components/marked-element/marked-element.html"/>
+
+    <link rel="import" href="bower_components/google-map/google-map.html"/>
+    <link rel="import" href="bower_components/google-map/google-map-marker.html"/>
+    <link rel="import" href="bower_components/google-map/google-map-poly.html"/>
+
+    <link rel="import" href="polymer-elements/copyright-statement.html"/>
+    <link rel="import" href="polymer-elements/glyphicon-social-icons.html"/>
+
+
+    <script type="text/javascript">
+      (function(){var p=[],w=window,d=document,e=f=0;p.push('ua='+encodeURIComponent(navigator.userAgent));e|=w.ActiveXObject?1:0;e|=w.opera?2:0;e|=w.chrome?4:0;
+      e|='getBoxObjectFor' in d || 'mozInnerScreenX' in w?8:0;e|=('WebKitCSSMatrix' in w||'WebKitPoint' in w||'webkitStorageInfo' in w||'webkitURL' in w)?16:0;
+      e|=(e&16&&({}.toString).toString().indexOf("\n")===-1)?32:0;p.push('e='+e);f|='sandbox' in d.createElement('iframe')?1:0;f|='WebSocket' in w?2:0;
+      f|=w.Worker?4:0;f|=w.applicationCache?8:0;f|=w.history && history.pushState?16:0;f|=d.documentElement.webkitRequestFullScreen?32:0;f|='FileReader' in w?64:0;
+      p.push('f='+f);p.push('r='+Math.random().toString(36).substring(7));p.push('w='+screen.width);p.push('h='+screen.height);var s=d.createElement('script');
+      s.src='bower_components/whichbrowser/detect.php?' + p.join('&');d.getElementsByTagName('head')[0].appendChild(s);})();
+      /*window.onerror = function(e) {
+      console.warn("Error thrown: "+e);
+      return true;
+      }*/
+    </script>
     <script type="text/javascript" src="https://maps.googleapis.com/maps/api/js?key=AIzaSyAZvQMkfFkbqNStlgzNjw1VOWBASd74gq4"></script>
     <script type="text/javascript" src="//ajax.googleapis.com/ajax/libs/jquery/2.1.3/jquery.min.js"></script>
     <script type="text/javascript" src="bower_components/bootstrap/dist/js/bootstrap.min.js"></script>
@@ -60,7 +123,7 @@ $loginStatus = getLoginState();
     <script type="text/javascript" src="bower_components/json-human/src/json.human.js"></script>
     <script type="text/javascript" src="bower_components/zeroclipboard/dist/ZeroClipboard.min.js"></script>
     <script type="text/javascript" src="js/c.min.js"></script>
-    <script type="text/javascript" src="js/project.min.js"></script>
+    <script type="text/javascript" src="js/project.js"></script>
     <script type="text/javascript">
       // Initial script
     </script>
@@ -76,14 +139,8 @@ $loginStatus = getLoginState();
       --paper-input-container-underline-disabled: var(--paper-grey-300);
       }
     </style>
-    <?php
-       require_once dirname(__FILE__) . "/modular/vulcanized-polymer.php";
-       ?>
   </head>
   <body class="container-fluid">
-    <?php
-       echo $vulcanized;
-       ?>
     <header id="header-bar" class="fixed-bar clearfix row">
       <p class="col-xs-12 login-status-bar text-right">
         <?php
