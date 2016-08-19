d2c480c8e6b17c2232d611953e358ede653aa5a2
Philip Kahn <tigerhawkvok@gmail.com>
2016-08-19 16:54:26 -0700

    sp->taxa

--- js/global-search.js
+++ js/global-search.js
@@ -243,7 +243,7 @@ doSearch = function(search, goDeep, hasRunValidated) {
         }
         console.warn("The search failed!");
         if (!isNull((ref = search.sampled_species) != null ? ref.data : void 0)) {
-          inputErrorHtml = "<span id=\"taxa-input-error\" class=\"help-block\">\n  Invalid species: Please check your spelling. <a href=\"http://amphibiaweb.org/search/index.html\" class=\"click\" data-newtab=\"true\">Check AmphibiaWeb for valid species</a>\n</span>";
+          inputErrorHtml = "<span id=\"taxa-input-error\" class=\"help-block\">\n  Invalid taxon: Please check your spelling. <a href=\"http://amphibiaweb.org/search/index.html\" class=\"click\" data-newtab=\"true\">Check AmphibiaWeb for valid taxa</a>\n</span>";
           if (isGoodSpecies) {
             inputErrorHtml = "<span id=\"taxa-input-error\" class=\"help-block\">\n  No matching samples found.\n</span>";
           }
