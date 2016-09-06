61dcdf75df8979dcf1917f414c93e21e38ed3e93
Philip Kahn <tigerhawkvok@gmail.com>
2016-09-05 18:08:33 -0700

    Smart helper text for #181

--- js/admin.js
+++ js/admin.js
@@ -2841,7 +2841,7 @@ showAddUserDialog = function(refAccessList) {
           $("#search-user").parent().find(".help-block").remove();
         } catch (undefined) {}
         return _adp.currentAsyncJqxhr = $.post(uri.urlString + "/api.php", "action=search_users&q=" + search, "json").done(function(result) {
-          var badge, bonusClass, helperHtml, html, l, len, prefix, users;
+          var badge, bonusClass, button, helperHtml, html, l, len, prefix, users;
           console.info(result);
           users = Object.toArray(result.result);
           if (users.length > 0) {
@@ -2894,7 +2894,12 @@ showAddUserDialog = function(refAccessList) {
             });
           } else {
             $("#user-search-result-container").prop("hidden", "hidden");
-            helperHtml = "<span class=\"help-block\">\n  We couldn't find a user matching \"" + search + "\".\n  <button class=\"btn btn-xs btn-default add-listed-user\">\n    Invite Them\n  </button>\n</span>";
+            try {
+              $("#search-user").parent().removeClass("has-error");
+              $("#search-user").parent().find(".help-block").remove();
+            } catch (undefined) {}
+            button = /(?:[a-z0-9!#$%&'*+\/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+\/=?^_`{|}~-]+)*|"(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21\x23-\x5b\x5d-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])*")@(?:(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?|\[(?:(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.){3}(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?|[a-z0-9-]*[a-z0-9]:(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21-\x5a\x53-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])+)\])\r\n/im.test(search) ? "<button class=\"btn btn-xs btn-default add-listed-user\"> Invite Them </button> " : "Finish the email address and we can invite them.";
+            helperHtml = "<span class=\"help-block\">\n  We couldn't find a user matching \"" + search + "\".\n  " + button + "\n</span>";
             $("#search-user").after(helperHtml);
             $("#search-user").parent().addClass("has-error");
             return $(".add-listed-user").click(function() {
