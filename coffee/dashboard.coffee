15eb212b8b1cc6c2b1dabbc7adac24f53595269f
Philip Kahn <tigerhawkvok@gmail.com>
2017-01-19 21:15:59 -0800

    Chart creator

--- coffee/dashboard.coffee
+++ coffee/dashboard.coffee
@@ -1,6 +1,28 @@
 
+createChart = (chartSelector, chartData, isSimpleData = false, appendTo = "main") ->
+  unless typeof chartData is "object"
+    console.error "Can't create a chart without a data object"
+  chartData.data ?= [1,2,3,4,5]
+  chartData.type ?= "bar"
+  chartData.labels ?= ["Label1", "Label2", "Label3", "Label4", "Label5"]
+  unless typeof chartData.options is "object"
+    chartData.options =
+      responsive: true
+  unless $(chartSelector).exists()
+    newId = if chartSelector.slice(0,1) is "#" then chartSelector.slice(1) else "dataChart-#{$("canvas").length}"
+    html = """
+    <canvas id="#{newId}" class="chart dynamic-chart">
+    </canvas>
+    """
+    $(appendTo).append html
+  ## Handle the chart
+  # http://www.chartjs.org/docs/
+  chartCtx = $(chartSelector)
+  chart = new Chart chartCtx, chartData
+
 
 
 $ ->
   console.log "Loaded dashboard"
+  createChart("#sample")
   false
--- js/dashboard.js
+++ js/dashboard.js
@@ -1,5 +1,42 @@
+var createChart;
+
+createChart = function(chartSelector, chartData, isSimpleData, appendTo) {
+  var chart, chartCtx, html, newId;
+  if (isSimpleData == null) {
+    isSimpleData = false;
+  }
+  if (appendTo == null) {
+    appendTo = "main";
+  }
+  if (typeof chartData !== "object") {
+    console.error("Can't create a chart without a data object");
+  }
+  if (chartData.data == null) {
+    chartData.data = [1, 2, 3, 4, 5];
+  }
+  if (chartData.type == null) {
+    chartData.type = "bar";
+  }
+  if (chartData.labels == null) {
+    chartData.labels = ["Label1", "Label2", "Label3", "Label4", "Label5"];
+  }
+  if (typeof chartData.options !== "object") {
+    chartData.options = {
+      responsive: true
+    };
+  }
+  if (!$(chartSelector).exists()) {
+    newId = chartSelector.slice(0, 1) === "#" ? chartSelector.slice(1) : "dataChart-" + ($("canvas").length);
+    html = "<canvas id=\"" + newId + "\" class=\"chart dynamic-chart\">\n</canvas>";
+    $(appendTo).append(html);
+  }
+  chartCtx = $(chartSelector);
+  return chart = new Chart(chartCtx, chartData);
+};
+
 $(function() {
   console.log("Loaded dashboard");
+  createChart("#sample");
   return false;
 });
 
