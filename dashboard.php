e8d0e3346f233634912799fe9f55660ee29e0b10
Philip Kahn <tigerhawkvok@gmail.com>
2017-08-23 17:18:45 -0700

    Fix #204

--- dashboard.php
+++ dashboard.php
@@ -576,7 +576,10 @@ if (toBool($_REQUEST["async"]) === true) {
             $speciesCount = mysqli_num_rows($r);
             $html = "";
             while ($row = mysqli_fetch_assoc($r)) {
-                $html .= "<p class='species-list-label'>".$row["genus"]." ".$row["specificepithet"]."</p><button class='btn btn-default species-list-button aweb-button'>AmphibiaWeb</button><button class='btn btn-default species-list-button data-summary-button'>Data Entry</button><br/>";
+                $html .= "<p class='species-list-label'>".$row["genus"]." ".$row["specificepithet"]."</p>
+<button class='btn btn-default species-list-button aweb-button click' data-href='http://amphibiaweb.org/cgi/amphib_query?rel-genus=equals&rel-species=equals&where-genus=".$row["genus"]."&where-species=".$row["specificepithet"]."' data-newtab='true'>AmphibiaWeb <iron-icon icon='icons:open-in-new'></iron-icon></button>
+<button class='btn btn-default species-list-button data-summary-button click' data-href='https://amphibiandisease.org/dashboard.php?taxon=".$row["genus"]."+".$row["specificepithet"]."'>Data Entry</button>
+<br/>";
             }
                 ?>
             <section class="col-xs-12 species-list"><?php echo $html; ?></section>
